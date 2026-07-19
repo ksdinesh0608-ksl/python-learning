c3ebd0ff8b7185bb6dfaf2b2dc31912182038726
Ks dinesh <ksdinesh0608@gmail.com>
2026-07-19 22:05:07 +0530

    Basics: Added calculator, temperature converter and simple interest

---
+++ Basics/celsius_to_fahrenheit.py
@@ -0,0 +1,3 @@
+c = int(input("Enter the Celsius temperature: "))
+f = (c * 9/5) + 32
+print("The Fahrenheit temperature is:", round(f, 2))
---
+++ Basics/simple_interest.py
@@ -0,0 +1,5 @@
+p = float(input("Enter the Principal Amount: "))
+r = float(input("Enter the Rate of Interest: "))
+t = float(input("Enter the Time (in years): "))
+si = (p * r * t) / 100
+print("Simple Interest =",si)
